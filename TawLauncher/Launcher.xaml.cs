d6aae0e6417a951194b1829755fb2271b9da1076
Pozitrone <jirirkas.skola@gmail.com>
2021-06-04 17:19:54 +0200

    Added test buttons

--- TawLauncher/Launcher.xaml.cs
+++ TawLauncher/Launcher.xaml.cs
@@ -3,15 +3,53 @@ using System.Windows;
 
 namespace TawLauncher
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class Launcher
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class Launcher
+  {
+    public Launcher()
     {
-        public MainWindow mainWindow;
-        public Launcher()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
     }
+
+    private void B1_OnClick(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        Version newVersion = UpdateCore.CheckForUpdates();
+        if (newVersion != null) B2.IsEnabled = true;
+        B1.IsEnabled = false;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.ToString());
+      }
+    }
+
+    private void B2_OnClick(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        UpdateCore.Update();
+        B2.IsEnabled = false;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.ToString());
+      }
+    }
+
+    private void B3_OnClick(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        UpdateCore.Run();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.ToString());
+      }
+    }
+  }
 }
