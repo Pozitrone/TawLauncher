e20edeea9981c60c2998214d3d3780a9a151e0a7
Pozitrone <jirirkas.skola@gmail.com>
2021-06-04 18:24:57 +0200

    Formatting; added all csproj links so I dont lose them... again

--- TawLauncher/Launcher.xaml.cs
+++ TawLauncher/Launcher.xaml.cs
@@ -3,53 +3,53 @@ using System.Windows;
 
 namespace TawLauncher
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class Launcher
-  {
-    public Launcher()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Launcher
     {
-      InitializeComponent();
-    }
+        public Launcher()
+        {
+            InitializeComponent();
+        }
 
-    private void B1_OnClick(object sender, RoutedEventArgs e)
-    {
-      try
-      {
-        Version newVersion = UpdateCore.CheckForUpdates();
-        if (newVersion != null) B2.IsEnabled = true;
-        B1.IsEnabled = false;
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show(ex.ToString());
-      }
-    }
+        private void B1_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Version newVersion = UpdateCore.CheckForUpdates();
+                if (newVersion != null) B2.IsEnabled = true;
+                B1.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
 
-    private void B2_OnClick(object sender, RoutedEventArgs e)
-    {
-      try
-      {
-        UpdateCore.Update();
-        B2.IsEnabled = false;
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show(ex.ToString());
-      }
-    }
+        private void B2_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                UpdateCore.Update();
+                B2.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
 
-    private void B3_OnClick(object sender, RoutedEventArgs e)
-    {
-      try
-      {
-        UpdateCore.Run();
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show(ex.ToString());
-      }
+        private void B3_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                UpdateCore.Run();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
     }
-  }
 }
