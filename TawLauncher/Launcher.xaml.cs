4fd5955bde73f5eef4f8fe197f7900a09d86e3c6
Pozitrone <jirirkas.skola@gmail.com>
2021-06-04 06:48:18 +0200

    Added launcher window

---
+++ TawLauncher/Launcher.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace TawLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Launcher
+    {
+        public MainWindow mainWindow;
+        public Launcher()
+        {
+            InitializeComponent();
+        }
+    }
+}
